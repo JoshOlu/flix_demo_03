e0c7d73b60f4cd0899f082e10557e68481ce3a9a
Josh Olumese <joshlume@fb.com>
2017-06-23 16:32:11 -0700

    Added Superhero tab to the tab bar that has a collection view of superhero movies. When tapped, a detail screen of that movie comes up.

---
+++ flix_demo_03/PosterCell.swift
@@ -0,0 +1,15 @@
+//
+//  PosterCell.swift
+//  flix_demo_03
+//
+//  Created by Josh Olumese on 6/23/17.
+//  Copyright © 2017 Josh Olumese. All rights reserved.
+//
+
+import UIKit
+
+class PosterCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var posterImageView: UIImageView!
+}
